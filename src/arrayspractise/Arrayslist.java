da7a65ab478baa20a4130b40c2827275a9dee9d0
sharmin@24 <sharminjchowdhury@gmail.com>
2024-12-12 23:28:14 -0500

    altered

--- src/arrayspractise/Arrayslist.java
+++ src/arrayspractise/Arrayslist.java
@@ -3,7 +3,7 @@ package arrayspractise;
 public class Arrayslist {
 
 	public static void main(String[] args) {
-		int[]x = {0,1,445,55,58,67,8};
+		int[]x = {0,1,445,55,58,67,8,23,45,67};
 		if(x[0]==1)
 		{for(int i= 0; i<=4;i++)
 			System.out.println(x[i]);
